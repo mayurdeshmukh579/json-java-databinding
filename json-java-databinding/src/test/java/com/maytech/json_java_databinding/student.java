9921e63bf609eccd0842695d472d2964c4a18793
mayurdeshmukh579 <mayurdeshmukh579@gmail.com>
2022-07-08 14:05:53 +0530

    sr02

--- json-java-databinding/src/test/java/com/maytech/json_java_databinding/student.java
+++ json-java-databinding/src/test/java/com/maytech/json_java_databinding/student.java
@@ -1,11 +1,22 @@
 package com.maytech.json_java_databinding;
 
+import java.util.Arrays;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+//@JsonIgnoreProperties(value = "isActive")
 public class student {
 	
 	private String name;
 	private String address;
 	private String city;
 	private int id;
+	private int[] phone;
+	@JsonProperty("isActive")
+	private boolean isActive;
+	private TrainerInfo batch;
+	
 	public String getName() {
 		return name;
 	}
@@ -30,10 +41,35 @@ public class student {
 	public void setId(int id) {
 		this.id = id;
 	}
+	public int[] getPhone() {
+		return phone;
+	}
+	public void setPhone(int[] phone) {
+		this.phone = phone;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	public TrainerInfo getBatch() {
+		return batch;
+	}
+	public void setBatch(TrainerInfo batch) {
+		this.batch = batch;
+	}
 	@Override
 	public String toString() {
-		return "student [name=" + name + ", address=" + address + ", city=" + city + ", id=" + id + "]";
+		return "student [name=" + name + ", address=" + address + ", city=" + city + ", id=" + id + ", phone="
+				+ Arrays.toString(phone) + ", isActive=" + isActive + ", batch=" + batch + "]";
 	}
+	
+	
+	
+	
+	
+	
 	
 	
 }
