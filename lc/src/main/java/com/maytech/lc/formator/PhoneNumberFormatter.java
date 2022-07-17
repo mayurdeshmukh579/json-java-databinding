e7182a3bd28f3a9edadaa05f081f95b94e8c241f
mayurdeshmukh579 <mayurdeshmukh579@gmail.com>
2022-07-17 14:14:09 +0530

    br-05

--- lc/src/main/java/com/maytech/lc/formator/PhoneNumberFormatter.java
+++ lc/src/main/java/com/maytech/lc/formator/PhoneNumberFormatter.java
@@ -7,7 +7,7 @@ import org.springframework.format.Formatter;
 
 import com.maytech.lc.api.Phone;
 
-public class PhoneNumberFormatter implements Formatter<Phone>{
+public class PhoneNumberFormatter implements Formatter<Phone> {
 
 	@Override
 	public String print(Phone object, Locale locale) {
@@ -18,17 +18,33 @@ public class PhoneNumberFormatter implements Formatter<Phone>{
 	@Override
 	public Phone parse(String completePhoneNumber, Locale locale) throws ParseException {
 		System.out.println("inside the parse method of PhoneFormatter");
-		
-		//Split the string received from user	
-		
+
+		// Whether number consist of '-'
+		int index = completePhoneNumber.indexOf('-');
+		Phone phone = new Phone();
 		String[] phoneNumber = completePhoneNumber.split("-");
-		
-		//Extract the countryCode and store it into phone class countryCodePoetry
-		
-		Phone phone = new Phone(); 
-		phone.setCountryCode(phoneNumber[0]);
-		phone.setPhoneNumber(phoneNumber[1]);
-		
+
+		if (index == -1 || completePhoneNumber.startsWith("-")) {
+			phone.setCountryCode("91");
+			if (completePhoneNumber.startsWith("-")) {
+				phone.setPhoneNumber(phoneNumber[1]);
+			} else {
+				phone.setPhoneNumber(phoneNumber[0]);
+
+			}
+
+		} // If number does not consist '-' inset bydefault '91-'
+
+		else {
+
+			// Split the string received from user
+
+			// Extract the countryCode and store it into phone class countryCodePoetry
+
+			phone.setCountryCode(phoneNumber[0]);
+			phone.setPhoneNumber(phoneNumber[1]);
+
+		}
 		return phone;
 	}
 
